07be6d412ef2522917b97aacfcc47674b12e6a18
marcverdoorn <marc.verdoorn@outlook.com>
2022-07-09 23:27:33 +0200

    added code to save hash in shared preferences

--- app/src/main/java/com/example/erebus/set_password.kt
+++ app/src/main/java/com/example/erebus/set_password.kt
@@ -1,5 +1,7 @@
 package com.example.erebus
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,6 +10,8 @@ import android.widget.Toast
 import java.security.MessageDigest
 
 class set_password : AppCompatActivity() {
+    private val sharedPrefFile = "EREBUS_data"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_password)
@@ -20,19 +24,29 @@ class set_password : AppCompatActivity() {
         val pass2 = pinp2.text.toString()
 
         if (pass1 == pass2 && pass1 != "" && pass2 != ""){
-            hash_pass(pass1)
+            var hash = hash_pass(pass1)
+            save_hash(hash)
         }else{
             Toast.makeText(applicationContext, "Re-confim password", Toast.LENGTH_SHORT).show()
         }
     }
 
-    fun hash_pass(input: String){
+    fun hash_pass(input: String): String{
         val bytes = MessageDigest.getInstance("SHA-1").digest(input.toByteArray())
         val hex_hash = bytes.toHex()
         Toast.makeText(applicationContext, hex_hash, Toast.LENGTH_SHORT).show()
+        return hex_hash
     }
 
     fun ByteArray.toHex(): String {
         return joinToString("") { "%02x".format(it) }
+    }
+
+    fun save_hash(input: String){
+        val sharedPreferences: SharedPreferences = this.getSharedPreferences(sharedPrefFile, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("hash", input)
+        editor.apply()
+        editor.commit()
     }
 }
