1c3f82af4cb14be8644a63d1694b0a6c854bda44
marcverdoorn <marc.verdoorn@outlook.com>
2022-07-23 22:31:14 +0200

    added note delete ability

--- app/src/main/java/com/example/erebus/viewnote.kt
+++ app/src/main/java/com/example/erebus/viewnote.kt
@@ -1,12 +1,14 @@
 package com.example.erebus
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 var filename: String = ""
 
@@ -18,15 +20,21 @@ class viewnote : AppCompatActivity() {
         val note_content = readfile(filename)
         val noteView = findViewById<EditText>(R.id.editTextTextMultiLine2)
         noteView.setText(note_content)
+        val title_view = findViewById<TextView>(R.id.title_view)
+        title_view.text = filename
     }
 
     fun readfile(filename: String): String{
-        val text = applicationContext.openFileInput(filename).bufferedReader().useLines { lines ->
-            lines.fold(""){ some, text ->
-                "$some\n$text"
-            }
-        }.toString()
-        return text
+        try {
+            val text = applicationContext.openFileInput(filename).bufferedReader().useLines { lines ->
+                lines.fold(""){ some, text ->
+                    "$some\n$text"
+                }
+            }.toString()
+            return text
+        }catch (e: Exception){
+            return e.toString()
+        }
     }
 
     fun save(view: View){
@@ -37,6 +45,8 @@ class viewnote : AppCompatActivity() {
                 applicationContext.openFileOutput(title, Context.MODE_PRIVATE).use{
                     it.write(plaintext.toByteArray())
                 }
+                intent = Intent(this, menu::class.java)
+                startActivity(intent)
             }catch (e: Exception){
                 Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show()
             }
@@ -46,6 +56,24 @@ class viewnote : AppCompatActivity() {
     }
 
     fun delete(view: View){
-
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Delete note")
+        builder.setMessage("Are you sure you want to delete this note?")
+        builder.setPositiveButton("YES"){
+            dialogInterface, which ->
+            try {
+                applicationContext.deleteFile(filename)
+                intent = Intent(this, menu::class.java)
+                startActivity(intent)
+            }catch (e: Exception){
+                Toast.makeText(applicationContext, e.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+        builder.setNegativeButton("NO"){
+            dialogInterface, which ->
+            Toast.makeText(applicationContext, "Didn't delete note", Toast.LENGTH_SHORT).show()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 }
